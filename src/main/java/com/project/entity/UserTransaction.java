6c4e4f8a404d39e56af2de35228699eed9d71fc6
Emerson Millard <emillard.code@gmail.com>
2022-08-02 12:00:00 -0700

    Added class.

---
+++ src/main/java/com/project/entity/UserTransaction.java
@@ -0,0 +1,22 @@
+package com.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTransaction {
+
+    @Id
+    private Integer orderId;
+
+    private int userId;
+    private int amount;
+
+}
