5f0193293655eb55cc92639598c9f59c8a83aa34
Emerson Millard <emillard.code@gmail.com>
2022-08-02 12:00:00 -0700

    Added class.

---
+++ src/main/java/com/project/entity/UserBalance.java
@@ -0,0 +1,21 @@
+package com.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserBalance {
+
+    @Id
+    private int userId;
+
+    private int price;
+
+}
