b3fffbc102fee6560a241d08550375a703632da5
Emerson Millard <emillard.code@gmail.com>
2022-08-03 12:00:00 -0700

    Added method.

--- src/main/java/com/project/service/PaymentService.java
+++ src/main/java/com/project/service/PaymentService.java
@@ -1,14 +1,22 @@
 package com.project.service;
 
+import com.project.dto.OrderRequestDTO;
+import com.project.dto.PaymentRequestDTO;
 import com.project.entity.UserBalance;
+import com.project.entity.UserTransaction;
+import com.project.event.OrderEvent;
+import com.project.event.PaymentEvent;
+import com.project.event.PaymentStatus;
 import com.project.repository.UserBalanceRepository;
 import com.project.repository.UserTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 
 @Service
 public class PaymentService {
@@ -29,6 +37,30 @@ public class PaymentService {
                 new UserBalance(104, 20000),
                 new UserBalance(105, 999)
         ).collect(Collectors.toList()));
+
+    }
+
+    /**
+     * // get the user id
+     * // check the balance availability
+     * // if balance sufficient -> Payment completed and deduct amount from DB
+     * // if payment not sufficient -> cancel order event and update the amount in DB
+     **/
+    @Transactional
+    public PaymentEvent newOrderEvent(OrderEvent orderEvent) {
+
+        OrderRequestDTO orderRequestDto = orderEvent.getOrderRequestDto();
+
+        PaymentRequestDTO paymentRequestDto = new PaymentRequestDTO(orderRequestDto.getOrderId(),
+                orderRequestDto.getUserId(), orderRequestDto.getAmount());
+
+        return userBalanceRepository.findById(orderRequestDto.getUserId())
+                .filter(ub -> ub.getPrice() > orderRequestDto.getAmount())
+                .map(ub -> {
+                    ub.setPrice(ub.getPrice() - orderRequestDto.getAmount());
+                    userTransactionRepository.save(new UserTransaction(orderRequestDto.getOrderId(), orderRequestDto.getUserId(), orderRequestDto.getAmount()));
+                    return new PaymentEvent(paymentRequestDto, PaymentStatus.PAYMENT_COMPLETED);
+                }).orElse(new PaymentEvent(paymentRequestDto, PaymentStatus.PAYMENT_FAILED));
 
     }
 
