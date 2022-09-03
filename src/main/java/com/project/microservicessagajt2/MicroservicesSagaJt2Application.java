873b03fb3e0e04f5ea0374107ad6b1ffed51750d
Emerson Millard <emillard.code@gmail.com>
2022-09-03 13:45:36 -0700

    Initial commit

---
+++ src/main/java/com/project/microservicessagajt2/MicroservicesSagaJt2Application.java
@@ -0,0 +1,13 @@
+package com.project.microservicessagajt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSagaJt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesSagaJt2Application.class, args);
+    }
+
+}
